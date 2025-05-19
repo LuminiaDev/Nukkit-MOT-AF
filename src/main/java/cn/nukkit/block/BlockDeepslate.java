ab96f694f41bc2943a0c9209f4fa07f28672fd77
Arkadiy <mefrreex@gmail.com>
2025-05-19 04:03:42 +0400

    fix: default state for basalt and deepslate

--- src/main/java/cn/nukkit/block/BlockDeepslate.java
+++ src/main/java/cn/nukkit/block/BlockDeepslate.java
@@ -2,6 +2,7 @@ package cn.nukkit.block;
 
 import cn.nukkit.Player;
 import cn.nukkit.item.Item;
+import cn.nukkit.item.ItemBlock;
 import cn.nukkit.item.ItemTool;
 import cn.nukkit.math.BlockFace;
 import cn.nukkit.utils.BlockColor;
@@ -67,6 +68,11 @@ public class BlockDeepslate extends BlockSolidMeta {
         }
 
         return new Item[]{this.toItem()};
+    }
+
+    @Override
+    public Item toItem() {
+        return new ItemBlock(this, 0);
     }
 
     @Override
