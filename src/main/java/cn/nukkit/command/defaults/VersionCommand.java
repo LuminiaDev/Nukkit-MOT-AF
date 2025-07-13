88185717d911178a6b58d07610bcee15272a0752
Arkadiy <mefrreex@gmail.com>
2025-07-13 16:37:28 +0400

    chore: cleanup imports

--- src/main/java/cn/nukkit/command/defaults/VersionCommand.java
+++ src/main/java/cn/nukkit/command/defaults/VersionCommand.java
@@ -9,15 +9,9 @@ import cn.nukkit.network.protocol.ProtocolInfo;
 import cn.nukkit.plugin.Plugin;
 import cn.nukkit.plugin.PluginDescription;
 import cn.nukkit.utils.TextFormat;
-import com.google.gson.JsonParser;
 
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
 import java.util.List;
 import java.util.Locale;
-import java.util.concurrent.CompletableFuture;
 
 /**
  * Created on 2015/11/12 by xtypr.
